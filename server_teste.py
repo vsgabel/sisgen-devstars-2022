49a837dc83583c283e70fa8d920be2ddf344c5d3
Victor Gabel <victor.gabel@gmail.com>
2022-05-07 18:44:18 -0300

    aula 06/05/2022

--- server_teste.py
+++ server_teste.py
@@ -1,54 +1,14 @@
-from flask import Flask, redirect, url_for, request
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "<h1>oi</h1>"
+    return render_template("index.html")
 
-@app.route("/nome/<nome>")
-@app.route("/nome")
-def nome(nome=None):
-    if nome:
-        return f"<h1>Bem-vindo, {nome}</h1>"
-    return "<h1>Seja bem-vindo</h1>"
-
-@app.route("/numero/<int:num>")
-def numero(num):
-    return f"O número é {num}"
-
-@app.route("/cliente/<int:idade>")
-@app.route("/cliente/<nome>")
-@app.route("/cliente/<int:idade>/<nome>")
-def nao_numero(idade=None, nome=None):
-    return f"Você digitou o texto {idade}"
-
-@app.route("/teste", methods=['GET', 'POST'])
-def teste():
-    data = request.form or {}
-
-    if data:
-        link = "http:\\\\" + data['link']
-        return redirect(link)
-
-    string = \
-    f"""
-    <html>
-        <body>
-            <form method="post">
-                <input type="text" name="link">
-                <input type="submit">
-            </form>
-        </body>
-    </html>
-    """
-
-    return string
-
-@app.route("/teste2")
-def teste2():
-    return redirect(url_for('index'))
-
+@app.route("/login")
+def login():
+    return render_template("login.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
