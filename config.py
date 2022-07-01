f69ab1af8eea5e9c49fb2e80ac48c127ff58ac8b
Victor Gabel <victor.gabel@gmail.com>
2022-06-30 21:15:47 -0300

    alteração postgres

--- config.py
+++ config.py
@@ -2,8 +2,8 @@ import os
 
 class Config:
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
-    if "postgre://" in SQLALCHEMY_DATABASE_URI:
-        SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgre://", "postgresql://")
+    if "postgres://" in SQLALCHEMY_DATABASE_URI:
+        SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     MAIL_SERVER = os.environ.get("MAIL_SERVER")
     MAIL_PORT = os.environ.get("MAIL_PORT")
