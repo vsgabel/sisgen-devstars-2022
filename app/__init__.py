68a9af43e6701a0d19569bd74721a2cf5c753187
Victor Gabel <victor.gabel@gmail.com>
2022-06-30 21:05:51 -0300

    inclusão do db

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,16 @@
 from flask import Flask
 from flask_mail import Mail
+from flask_sqlalchemy import SQLAlchemy
 from config import config
 
 mail = Mail()
+db = SQLAlchemy()
 
 def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config[config_name])
+
+    db.init_app(app)
     mail.init_app(app)
 
     from app.main import main as main_bp
--- sisgen.py
+++ sisgen.py
@@ -1,7 +1,10 @@
 import os
-from app import create_app
+from app import create_app, db
+from flask_migrate import Migrate
+from app.models import * 
 
 app = create_app(os.environ.get('FLASK_CONFIG') or 'default')
+migrate = Migrate(app, db)
 
 if __name__ == "__main__":
     app.run()
